990dfabd04f35ace2ac61e596de07b683b091a65
James Gitting <james.gitting@gmail.com>
2018-06-25 15:26:40 -0700

    ReporteRs starter stub

---
+++ reporters/script_reporters.R
@@ -0,0 +1,37 @@
+####################################################################################################
+#                                                                                                  #
+#       Sample Script to auto-populate powerpoint presentations with a powerpoint template         #
+#                                                                                                  #
+#       This version uses the ReporteRs package and alternatively, there's a package called        #
+#       officer by the same author, but drops the use of rJava and produces native Microsoft       #
+#       charts. The ReporteRs project is being sunsetted in favor of officer.                      #
+#                                                                                                  #
+#       Despite that fact, I'm still including this example and will use this workflow until       #
+#       the workflow breaks.  Post-report generation manipulation is currently a much nicer        #
+#       experience using this package than the officer package.                                    #
+#                                                                                                  #
+####################################################################################################
+
+# Libraries -----------------------------------------------------------------------------------
+library(ReporteRs)
+library(tidyverse)
+library(lubridate)
+library(zoo)
+
+library(ggplot2)
+library(ggthemes)
+library(scales)
+
+# Load Data -----------------------------------------------------------------------------------
+
+
+
+
+# Theme Settings ------------------------------------------------------------------------------
+
+
+
+
+# Generate Reports ----------------------------------------------------------------------------
+
+
