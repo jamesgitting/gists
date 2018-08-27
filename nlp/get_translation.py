3f4a3346a13fc03cf7ac6e46493eb8cc24114b83
James Truong <james.gitting@gmail.com>
2018-08-27 13:37:55 -0700

    Adding skeleton script for translation gist

---
+++ nlp/get_translation.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+import argparse
+
+from googletrans import Translator
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+def main(opt):
+    pass
+
+
+def comment_eval(x, target_lang='en'):
+    translator = Translator()
+    sentimenter = SentimentIntensityAnalyzer()
+
+    detect_lang = translator.detect(x['comment_text'])
+    sentiment = sentimenter.polarity_scores(x['comment_text'])
+
+    lang = detect_lang.lang
+    confidence = detect_lang.confidence
+    translation = translator.translate(x['comment_text'], dest=target_lang).text
+    compound = sentiment['compound']
+    neutral_score = sentiment['neu']
+    negative_score = sentiment['neg']
+    positive_score = sentiment['pos']
+
+    return lang, confidence, translation, compound, neutral_score, negative_score, positive_score
+
+if "__name__" = "__main__":
+    parser = argparse.ArgumentParser(description = 'Process comments')
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    required.add_argument('-f', '--file', help='Input file path', required=True)
+    optional.add_argument('-o', '--output', help='Output file path', default='stdout')
+
+    opt = parser.parse_args()
+    main(opt)
